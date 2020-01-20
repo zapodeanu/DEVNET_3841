c7a0f15509144d0e3af14ed16a3d7d016f49683e
Gabriel Zapodeanu <gzapodea@cisco.com>
2020-01-19 19:57:54 -0800

    initial commit

---
+++ config.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+Copyright (c) 2019 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+"""
+
+__author__ = "Gabriel Zapodeanu TME, ENB"
+__email__ = "gzapodea@cisco.com"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2019 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+# This file contains the:
+# Webex Teams Auth and URL
+# DNAC username and password, server info
+# ServiceNow developer instance info, username and password
+# PubNub Keys
+
+# Update this section with your PubNub keys
+PUB_KEY = ''
+SUB_KEY = ''
+SEC_KEY = ''
+CHANNEL = ''
+
+
+# Update this section with the Webeex Teams token for each student
+WEBEX_TEAMS_URL = 'https://api.ciscospark.com/v1'
+WEBEX_TEAMS_AUTH = 'Bearer ' + ''
+WEBEX_TEAMS_SPACE = ''  # to be used when new instance created in ServiceNow
+
+
+# Update this section with the DNA Center server info and user information
+DNAC_URL = ''
+DNAC_USER = ''
+DNAC_PASS = ''
+
+# Update this section with the Service Now instance to be used for labs
+SNOW_URL = 'https://devxxxx.service-now.com/api/now'
+SNOW_ADMIN = ''
+SNOW_DEV = ''
+SNOW_PASS = ''
+SNOW_INSTANCE = 'devxxxx'
+
+
+# Update this section with the info for the CSR1000v to be used during the IOS XE Module
+IOS_XE_HOST = ''
+IOS_XE_USER = ''
+IOS_XE_PASS = ''
+IOS_XE_PORT = 830
+
+
+FOLDER_NAME = '/bootflash/DEVNET_3841'
+
+# Cisco DNA Center project and template
+DNAC_PROJECT = ''
+DNAC_CLI_TEMPLATE = ''
